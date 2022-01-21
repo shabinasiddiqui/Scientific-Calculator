2cf9a862ab11573d46f1bf37531c5e6f040e623d
Shabina Siddiqui <siddiquishabina10@gmail.com>
2022-01-21 17:27:58 +0530

    Added functions

---
+++ index.js
@@ -0,0 +1,44 @@
+var screen = document.querySelector('#screen');
+var btn = document.querySelectorAll('.btn');
+for (item of btn) {
+    item.addEventListener('click', (e) => {
+        btntext = e.target.innerText;
+        if (btntext == 'x') {
+            btntext = '*';
+        }
+        screen.value += btntext;
+
+    });
+}
+
+function sin() {
+    screen.value = Math.sin(screen.value);
+}
+
+function cos() {
+    screen.value = Math.cos(screen.value);
+}
+
+function tan() {
+    screen.value = Math.tan(screen.value);
+}
+
+function pow() {
+    screen.value = Math.sin(screen.value, 2);
+}
+
+function sqrt() {
+    screen.value = Math.sqrt(screen.value, 2);
+}
+
+function log() {
+    screen.value = Math.log(screen.value, 2);
+}
+
+function pi() {
+    screen.value = 3.14159;
+}
+
+function e() {
+    screen.value = 2.718;
+}
